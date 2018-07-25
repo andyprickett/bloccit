a22a76a087c344aa4411ae6210807673d2024420
Andy Prickett <andy@andyprickett.com>
2018-07-24 21:11:22 -0700

    WIP deploy to production

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ const app = express();
 //app.use("/", (req, res, next) => {
 //  res.send("Welcome to Bloccit")
 //});
+const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
 
+appConfig.init();
 routeConfig.init(app);
 
 module.exports = app;
