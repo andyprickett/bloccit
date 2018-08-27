7706d95efbaefd6d19ffac679389974af79efdc5
Andy Prickett <andy@andyprickett.com>
2018-08-27 10:58:24 -0700

    Checkpoint 13 - Comments: completed

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -32,14 +32,24 @@ module.exports = {
       req.checkBody("password", "must be at least 6 characters in length").isLength({min: 6})
       req.checkBody("passwordConfirmation", "must match password provided").optional().matches(req.body.password);
     }
-
     const errors = req.validationErrors();
-
     if(errors) {
       req.flash("error", errors);
       return res.redirect(req.headers.referer);
     } else {
       return next();
+    }
+  },
+  validateComments(req, res, next) {
+    if(req.method === "POST") {
+      req.checkBody("body", "must not be empty").notEmpty();
+    }
+    const errors = req.validationErrors();
+    if(errors) {
+      req.flash("error", errors);
+      return res.redirect(req.headers.referer);
+    } else {
+      return next()
     }
   }
 }
